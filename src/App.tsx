6a14324cd832ea02783f5cc808d03839425545e3
SoloTufi <mat_990011@mail.ru>
2024-03-20 00:13:48 +0700

    layout
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import './scss/App.scss';
+import CloudyIcon from './components/cicons/CloudyIcon';
+import SearchIcon from './components/cicons/SearchIcon';
+import style from './scss/App.module.scss';
 
 function App() {
-	return <div className='app'></div>;
+	return (
+		<div className={style.root}>
+			<div className={style.container}>
+				<form action='' className={style.form}>
+					<input type='text' className={style.input} />
+					<button type='submit' className={style.btnSearch}>
+						<SearchIcon />
+					</button>
+				</form>
+				<article className={style.card}>
+					<h1 className={style.city}>Barnaul</h1>
+					<span className={style.date}>Tuesday, March 19, 2024</span>
+					<div className={style.info}>
+						<div className={style.status}>
+							<CloudyIcon />
+						</div>
+						<div className={style.temp}>
+							<span>5 C°</span>
+						</div>
+					</div>
+				</article>
+			</div>
+		</div>
+	);
 }
 
 export default App;
