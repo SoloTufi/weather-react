d414def7356cbcda8f3a7b777d931fa2be999175
SoloTufi <mat_990011@mail.ru>
2024-03-20 18:26:18 +0700

    Search logic
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,23 @@
+import React from 'react';
+
 import style from './search.module.scss';
 
-import SearchIcon from '../cicons/SearchIcon';
+import SearchIcon from '../Cicons/SearchIcon';
 
-const Search = () => {
+const Search: React.FC = () => {
+	const [searchValue, setSearchValue] = React.useState<string>('');
+	const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(event.target.value);
+	};
+
 	return (
 		<form action='' className={style.form}>
-			<input type='text' className={style.input} />
+			<input
+				type='text'
+				value={searchValue}
+				onChange={onChangeInput}
+				className={style.input}
+			/>
 			<button type='submit' className={style.btnSearch}>
 				<SearchIcon />
 			</button>
