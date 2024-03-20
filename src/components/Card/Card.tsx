2a6420b1ff71f742844caf32d53c257e7141413f
SoloTufi <mat_990011@mail.ru>
2024-03-20 18:26:52 +0700

    #
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,21 @@
-import CloudyIcon from '../cicons/CloudyIcon';
-import style from './card.module.scss';
+import React from 'react';
 
-const Card = () => {
+import style from './card.module.scss';
+import { CloudyIcon, RainIcon, ShtormIcon, SnowfallIcon, SunnyIcon } from '../Cicons/StatusIcons';
+
+const Card: React.FC = () => {
 	return (
 		<article className={style.card}>
 			<h1 className={style.city}>Barnaul</h1>
 			<span className={style.date}>Tuesday, March 19, 2024</span>
 			<div className={style.info}>
 				<div className={style.status}>
-					<CloudyIcon />
+					<RainIcon />
 				</div>
 				<div className={style.temp}>
 					<span>5 C°</span>
 				</div>
+				<span className={style.desc}>Cloudy, no precipitation</span>
 			</div>
 		</article>
 	);
